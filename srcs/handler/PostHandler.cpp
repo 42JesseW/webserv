de85eba95bed2f5fa04cead3ed463adba4af5e5f
Katherine <katherine-fu@outlook.com>
2022-04-18 17:20:18 +0200

    comment

--- srcs/handler/PostHandler.cpp
+++ srcs/handler/PostHandler.cpp
@@ -9,11 +9,16 @@ bool Handler::post_handler(Request &request, std::string uploadPath)
     {
         filename = uploadPath + request.getFilename();
     }
-    else
+    else if (request.getHeaders().find("Content-Type") != request.getHeaders().end())
     {
         std::string extension = request.getHeaders().find("Content-Type")->second;
         extension = extension.substr(extension.find('/') + 1);
         filename = "test." + extension;
+    }
+    else
+    {
+        request.setStatus(HTTP_STATUS_BAD_REQUEST);
+        return (false);
     }
     newFile.open(uploadPath + filename, std::ofstream::out | std::ofstream::binary);
     if (!newFile.is_open())
