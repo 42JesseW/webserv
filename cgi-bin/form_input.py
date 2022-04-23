b82ed804c19eaa34e0cc50b8c8a69a108850d916
42JesseW <64128114+42JesseW@users.noreply.github.com>
2022-04-23 14:37:40 +0200

    Update form_input.py
    Old version is needed for test_cgi.cpp

--- cgi-bin/form_input.py
+++ cgi-bin/form_input.py
@@ -1,9 +1,18 @@
-#!.venv/bin/python
+#!/Users/jevan-de/Documents/projects/webserv/webserv_github/.venv/bin/python
 
-print("Content-Type: text/html");
-print("Content-Length: 90\n");
-print("<html>");
-print("<body>");
-print("<h1>Form input!</h1>");
-print("</body>");
-print("</html>");
+import cgi
+import cgitb
+
+cgitb.enable()
+
+input_data = cgi.FieldStorage()
+
+print('Content-Type:text/html\r\n\r\n')
+print('<h1>Addition Results</h1>')
+try:
+    num1 = int(input_data["num1"].value)
+    num2 = int(input_data["num2"].value)
+except Exception as e:
+    print('<output>Sorry, the script cannot turn your inputs into numbers (integers).</output>')
+    raise SystemExit(1)
+print('<output>{0} + {1} = {2}</output>'.format(num1, num2, num1 + num2))
