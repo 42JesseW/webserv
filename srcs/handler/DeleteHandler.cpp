79d1f9973de40a20e57a211f2877b7f3278738f4
Katherine <katherine-fu@outlook.com>
2022-05-01 12:55:10 +0200

    commit

--- srcs/handler/DeleteHandler.cpp
+++ srcs/handler/DeleteHandler.cpp
@@ -2,28 +2,6 @@
 
 bool Handler::delete_handler(Request &request, std::string searchPath)
 {
-    std::string filename;
-
-    filename = searchPath + request.getFilename();
-    if (request.getFilename().empty())
-    {
-        request.setStatus(HTTP_STATUS_BAD_REQUEST);
-        return (false);
-    }
-    if (!filename.empty())
-    {
-        if (std::remove(filename.c_str()) != 0)
-        {
-            request.setStatus(HTTP_STATUS_NOT_FOUND);
-            return (false);
-        }
-    }
-    return (true);
-}
-#include <Handler.hpp>
-
-bool Handler::delete_handler(Request &request, std::string searchPath)
-{
     std::string     filename;
     std::fstream    fileStream;
 
